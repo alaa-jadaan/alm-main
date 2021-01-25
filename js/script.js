0ea006d36419f9c1d42e007f2637dae303250530
alaajad01 <alaajad.01@gmail.com>
2021-01-26 00:31:15 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(function () {
+    
+
+    if ($(window).outerWidth() <= 991.98) {
+//        $("#top-nav").addClass("navbar-dark bg-dark").removeClass("navbar-light");
+        
+        $("#subjects .card").click(function () {
+            if($(this).find(".details").css("display") == "none"){
+                $("#subjects .card .details").slideUp();
+                $("#subjects .card").removeClass("open");
+                $(this).find(".details").stop(true, true).slideDown();
+                $(this).addClass("open");
+            }else{
+                $(this).find(".details").stop(true, true).slideUp();
+                $(this).removeClass("open");
+            }
+            
+        });
+    } else {
+//        $("#top-nav").removeClass("navbar-dark bg-dark").addClass("navbar-light");
+        
+        $("#subjects .card").hover(function () {
+            $(this).find(".details").slideDown();
+        }, function () {
+            $(this).find(".details").stop(true, true).slideUp();
+        });
+    }
+
+    $(window).resize(function () {
+        $("#subjects .card .details").slideUp();
+        $("#subjects .card").removeClass("open");
+        if ($(window).outerWidth() <= 991.98) {
+//            $("#top-nav").addClass("navbar-dark bg-dark").removeClass("navbar-light");
+            
+            $("#subjects .card").unbind();
+            $("#subjects .card").click(function () {
+                if($(this).find(".details").css("display") == "none"){
+                    $("#subjects .card .details").slideUp();
+                    $("#subjects .card").removeClass("open");
+                    $(this).find(".details").stop(true, true).slideDown();
+                    $(this).addClass("open");
+                }else{
+                    $(this).find(".details").stop(true, true).slideUp();
+                    $(this).removeClass("open");
+                }
+            });
+        } else {
+//            $("#top-nav").removeClass("navbar-dark bg-dark").addClass("navbar-light");
+            
+            $("#subjects .card").unbind();
+            $("#subjects .card").hover(function () {
+                $(this).find(".details").slideDown();
+            }, function () {
+                $(this).find(".details").stop(true, true).slideUp();
+            });
+        }
+    });
+    
+    $('#top-nav .collapse').on('show.bs.collapse', function () {
+        $("#top-nav").addClass("navbar-dark bg-dark").removeClass("navbar-light");
+    });
+    
+    $('#top-nav .collapse').on('hidden.bs.collapse', function () {
+        $("#top-nav").removeClass("navbar-dark bg-dark").addClass("navbar-light");
+    });
+    
+    
+});
