3944013530c0d7f98415f530382ccaf45e25af13
alaajad01 <alaajad.01@gmail.com>
2021-02-28 07:37:37 +0200

    hadeeth40 page and fixes
    fixes on main page style and slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,43 @@
 $(function () {
-    
+    //Replace all SVG images with inline SVG
+    function imgToSvg() {
+        $('img.svg').each(function () {
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+
+            jQuery.get(imgURL, function (data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+
+                // Add replaced image's ID to the new SVG
+                if (typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if (typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                }
+
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+
+                // Check if the viewport is set, else we gonna set it if we can.
+                if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+                }
+
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+
+            }, 'xml');
+
+        });
+    }
+
+    //convert svg images on page first load
+    imgToSvg();
 
     if ($(window).outerWidth() <= 991.98) {
 //        $("#top-nav").addClass("navbar-dark bg-dark").removeClass("navbar-light");
